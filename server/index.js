83e4c4287fb95a51f0a36f7844c38b3e3b1027b5
hemanthkumar <hemanthkumarr21cse@srishakthi.ac.in>
2025-07-04 18:16:00 +0530

    changes 4

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('✅ MongoDB connected'))
+.then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err.message));
 
 const app = express();
