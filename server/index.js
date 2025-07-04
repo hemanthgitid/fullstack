d0496dcf6bf24cd58546b4ad465798534e75b778
hemanthkumar <hemanthkumarr21cse@srishakthi.ac.in>
2025-07-04 18:15:10 +0530

    changes 3

--- server/index.js
+++ server/index.js
@@ -5,13 +5,12 @@ const Student = require('./models/student');
 const Course = require('./models/course.js'); 
 require('dotenv').config();
 
-// mongoose.connect(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('✅ MongoDB connected'))
-.catch(err => console.error('❌ MongoDB connection error:', err.message));
+.catch(err => console.error('MongoDB connection error:', err.message));
 
 const app = express();
 app.use(cors());
